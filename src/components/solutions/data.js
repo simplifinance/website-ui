d294a85ca4a74f391affc151f723141d06e49322
bobeu <bobmatea27@gmail.com>
2024-10-01 18:58:11 +0100

    FIxex incorrect words

--- src/components/solutions/data.js
+++ src/components/solutions/data.js
@@ -8,30 +8,25 @@ export const solutionData = [
     paragraph1: (
       <p className="">
         Our FlexPool derives its name from the flexible options it offers 
-        to users. Exisitng liquidity protocols have a unified liquidity
-        pool created by the protocol where loans are sourced. In contrast, 
-        FlexPools is a combination of several liquidity pools created by users.
+        to users. It is a combination of several liquidity pools created by users
+        for peer-funding
       </p>
     ),
     paragraph2: (
       <p className="">
-        Existing liquidity pools (_ELP_) are controlled and watched by the team 
-        or DAO but FlexPools are controlled and run by users. 
+        Easy access to a large and expandable pool of funds
       </p>
     ),
     paragraph3: (
       <p className="">
-        ELP have a single point of failure during security breachs or in the event 
-        where potential vulnerability is exploited which may lead to loss of funds 
-        that may significantly put the protocol on its kneel. The permissioned and 
-        permissionless nature of FlexPool brings greater decentralization and sense 
-        of responsibility to its participants since they're acting as the providers 
-        and borrowers at the same time. A bad actor in a permissioned pool will be 
-        known ahead by the creator and the chance of succeding is very tiny and almost
-         impossible. In a permissionless FlexPool, same sense of responsibity is required except the providers may not be known.
+        Our FlexPools bring true decentralization, control, and sense 
+        of responsibility to users since participants are providers 
+        and borrowers. A bad actor in a FlexPool pool can easily be 
+        identified, and the chance of succeeding is very tiny and almost
+        impossible.
       </p>
     ),
-    sectionImage: "images/frame2.svg",
+    sectionImage: "images/frame2.svg",  
     sectionImage2: "images/frame3.svg",
     sectionImage3: "images/frame4.svg",
     sectionName: "Solutions we offer",
